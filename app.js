2f6cd834d5bbd8ae091880eca9afcab47fa6544c
Franco Calegari <francocalegari55@gmail.com>
2025-10-31 10:42:39 -0300

    fix

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const morgan = require("morgan");
 const session = require("express-session");
 const bodyParser = require("body-parser");
 
+const publicRoutes = require("./routes/publicRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+const servicesAPI = require("./routes/api/services");
+const advantagesAPI = require("./routes/api/advantages");
+const costsAPI = require("./routes/api/costs");
+const productosAPI = require("./routes/api/productos");
+const imagesAPI = require("./routes/api/images");
+
 const app = express();
 
 // ===== Middleware =====
@@ -32,6 +40,13 @@ app.use("/api/costs", costsAPI);
 app.use("/api/productos", productosAPI);
 app.use("/api/images", imagesAPI);
 
+// ===== Servidor =====
+const PORT = process.env.PORT || 3000;
+if (require.main === module) {
+	app.listen(PORT, () => {
+		console.log(`Servidor escuchando en http://localhost:${PORT}`);
+	});
+}
+
 // ✅ Exportar express para Vercel
 module.exports = app;
-
